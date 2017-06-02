15973868f36f2014f62458a7ae233e9d5606b7d2
Sizhao Yang <zaoyang@gmail.com>
2017-06-02 16:13:21 -0700

    reorganized everything

--- orig/utils.py
+++ orig/utils.py
@@ -1,55 +1,16 @@
 from __future__ import division,print_function
-import math, os, json, sys, re
-import cPickle as pickle
-from glob import glob
-import numpy as np
-from matplotlib import pyplot as plt
-from operator import itemgetter, attrgetter, methodcaller
-from collections import OrderedDict
+
 import itertools
-from itertools import chain
+import math
 
-import pandas as pd
-import PIL
-from PIL import Image
-from numpy.random import random, permutation, randn, normal, uniform, choice
-from numpy import newaxis
-import scipy
-from scipy import misc, ndimage
-from scipy.ndimage.interpolation import zoom
-from scipy.ndimage import imread
-from sklearn.metrics import confusion_matrix
 import bcolz
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.manifold import TSNE
-
-from IPython.lib.display import FileLink
-
-import theano
-from theano import shared, tensor as T
-from theano.tensor.nnet import conv2d, nnet
-from theano.tensor.signal import pool
-
-import keras
-from keras import backend as K
-from keras.utils.data_utils import get_file
-from keras.utils import np_utils
-from keras.utils.np_utils import to_categorical
-from keras.models import Sequential, Model
-from keras.layers import Input, Embedding, Reshape, merge, LSTM, Bidirectional
-from keras.layers import TimeDistributed, Activation, SimpleRNN, GRU
-from keras.layers.core import Flatten, Dense, Dropout, Lambda
-from keras.regularizers import l2, activity_l2, l1, activity_l1
-from keras.layers.normalization import BatchNormalization
-from keras.optimizers import SGD, RMSprop, Adam
 from keras.utils.layer_utils import layer_from_config
-from keras.metrics import categorical_crossentropy, categorical_accuracy
-from keras.layers.convolutional import *
-from keras.preprocessing import image, sequence
-from keras.preprocessing.text import Tokenizer
-
+from keras.utils.np_utils import to_categorical
+from matplotlib import pyplot as plt
 from vgg16 import *
-from vgg16bn import *
+
+from orig.vgg16bn import *
+
 np.set_printoptions(precision=4, linewidth=100)
 
 
